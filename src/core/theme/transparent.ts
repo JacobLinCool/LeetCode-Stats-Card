c7d34976d7fd5f17603b5ff2821fd75c2257c9ae
Iyehah Hacen <117352746+iyehah@users.noreply.github.com>
2024-08-11 16:07:36 +0000

    Update transparent.ts

--- src/core/theme/transparent.ts
+++ src/core/theme/transparent.ts
@@ -2,8 +2,8 @@ import { Theme } from "./_theme";
 
 export default Theme({
     palette: {
-        bg: ["rgba(16, 16, 16, 0.5)", "rgba(0, 0, 0, 0.5)"],
-        text: ["#417E87", "#417E87"],
+        bg: ["rgba(225, 225, 225,0)"],
+        text: ["#417E87"],
         color: ["#ffa116", "#5cb85c", "#f0ad4e", "#d9534f"],
     },
     css: `#L{fill:#fff}`,
