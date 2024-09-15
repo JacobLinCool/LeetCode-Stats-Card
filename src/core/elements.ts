990ba6e2da233972b2f4116489178069f0f78c77
Wisesofthemall <foxxydieujuste@gmail.com>
2024-09-15 16:34:06 -0400

    finished

--- src/core/elements.ts
+++ src/core/elements.ts
@@ -2,8 +2,8 @@ import { Item, svg_attrs } from "./item";
 import { Config, FetchedData } from "./types";
 
 export function Root(config: Config, data: FetchedData) {
-    config.width = Math.max(config.width, 500);
-    config.height = Math.max(config.height, 800);
+    config.width = Math.max(config.width, 1000);
+    config.height = Math.max(config.height, 400);
     return new Item("svg", {
         id: "root",
         attr: {
